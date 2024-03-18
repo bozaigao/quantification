fb03a8af4d1738320c1e7663fbd5304d61fd6fde
Yanbo He <Yanbo.He-Vendor@hbc.com>
2024-03-18 19:25:09 +0800

    fix: update

--- backtest/utils/opening_increase.py
+++ backtest/utils/opening_increase.py
@@ -10,6 +10,8 @@ def getOpeningIncrease(browserTab, date, name):
     div_text = soup.find('div', class_='jgy_txt_isLayout').get_text(strip=True)
     paragraphs = soup.find_all('p', recursive=False)
     match = re.search(r'竞价涨幅(-?\d+\.\d+%)', div_text)
+    if not match:
+        match = re.search(r'竞价涨幅为(-?\d+\.\d+%)', div_text)
     if match:
         percentage_text = match.group(1)
     else:
--- opening_increase.py
+++ opening_increase.py
@@ -38,6 +38,8 @@ for idx, workday in enumerate(dates):
         div_text = soup.find('div', class_='jgy_txt_isLayout').get_text(strip=True)
         paragraphs = soup.find_all('p', recursive=False)
         match = re.search(r'竞价涨幅(-?\d+\.\d+%)', div_text)
+        if not match:
+           match = re.search(r'竞价涨幅为(-?\d+\.\d+%)', div_text)
         if match:
             percentage_text = match.group(1)
         else:
