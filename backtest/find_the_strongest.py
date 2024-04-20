9e6ad4189a38ae3b80e189c1e9ebe9d88eaa5b86
Yanbo He <Yanbo.He-Vendor@hbc.com>
2024-04-20 17:07:11 +0800

    fix: update

--- backtest/find_the_strongest.py
+++ backtest/find_the_strongest.py
@@ -29,27 +29,31 @@ try:
 except FileNotFoundError:
     stocks_data = []
 # find_date = datetime.now().date()
-find_date = datetime.strptime('2024-03-19', '%Y-%m-%d').date()
-# pre_date = '2024-01-12'
+find_date = datetime.strptime('2024-04-02', '%Y-%m-%d').date()
+# pre_date = '2024-03-22'
 pre_date = calendar.valid_days(start_date=find_date + timedelta(days=-1), end_date='2100-01-01')[0].date()
 print(f'findDate:{str(find_date)},preDate:{str(pre_date)}')
 for item in stocks_data:
-    if item['date'] == str(find_date):
+    if item['date'] == str(pre_date):
         for item2 in item['data']:
-            if item2['limit'] > 2:
+            if item2['limit'] > 1:
                 pre_opening_increase = float(getOpeningIncrease(browserTab,str(pre_date),item2['code'])[0].strip('%'))
                 current_opening_increase = float(getOpeningIncrease(browserTab,str(find_date),item2['code'])[0].strip('%'))
+                print(f'{item2["name"]},{pre_opening_increase}--{current_opening_increase}')
                 if pre_opening_increase >= 9.5 and current_opening_increase >= 9.5 and abs(pre_opening_increase - current_opening_increase) <= 0.5:
                     bothIsLimitPrice = True
                 else:
                     bothIsLimitPrice = False
                 if current_opening_increase > pre_opening_increase or bothIsLimitPrice:
-                    strongest_pool.append({'date':str(find_date),'name':item2['name'],'desc':f'昨日竞价{pre_opening_increase}%,当日竞价{current_opening_increase}%','limit':item2['limit']})
+                   strongest_pool.append({'date':str(find_date),'name':item2['name'],'pre_opening_increase':pre_opening_increase,'current_opening_increase':current_opening_increase,'limit':item2['limit']})
 
+strongest_pool = sorted(strongest_pool, key=lambda x: (-x['limit'], -x['current_opening_increase']))
 for index, item in enumerate(strongest_pool):
     if item["limit"] > 2:
-       print(Fore.RED + f'{index+1}.{item["name"]},{item["desc"]},{item["limit"]}板')
+       print(Fore.RED + f'{index+1}.{item["name"]},昨日竞价{item["pre_opening_increase"]}%,当日竞价{item["current_opening_increase"]}%,{item["limit"]}板, 振幅{round(abs(item["current_opening_increase"] - item["pre_opening_increase"]),2)}%')
     else:
-       print(Fore.GREEN + f'{index+1}.{item["name"]},{item["desc"]},{item["limit"]}板')
+       print(Fore.GREEN + f'{index+1}.{item["name"]},昨日竞价{item["pre_opening_increase"]}%,当日竞价{item["current_opening_increase"]}%,{item["limit"]}板, 振幅{round(abs(item["current_opening_increase"] - item["pre_opening_increase"]),2)}%')
 
+# with open(f'{os.getcwd().replace("/backtest", "")}/backtest/{year}_test_data.json', 'w') as file:
+#         json.dump(strongest_pool, file,ensure_ascii=False,  indent=4) 
 
