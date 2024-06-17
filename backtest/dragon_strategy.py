f83e6d1ce6a03d0cb2d06125a44a8005af2d8b9f
Yanbo He <Yanbo.He-Vendor@hbc.com>
2024-06-17 10:22:04 +0800

    fix: update

--- backtest/dragon_strategy.py
+++ backtest/dragon_strategy.py
@@ -428,7 +428,7 @@ def strategy(pre_date,date):
 #        break
 if forecast:
 #    strategy(str(date_object),str(today))
-   strategy('2024-02-20','2024-02-21')
+   strategy('2024-06-14','2024-06-17')
 else:
     for idx, date in enumerate(dates[1:]):
         strategy(str(get_previous_trading_day(datetime.strptime(date, '%Y-%m-%d').date())),date)
