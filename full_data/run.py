e0c8c291628f7f230a503a61a065a4f9b78ae3e1
Yanbo He <Yanbo.He-Vendor@hbc.com>
2024-07-13 22:03:22 +0800

    fix: update

---
+++ full_data/run.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+
+# Python脚本路径
+script_path = "/Users/yanbo.he/Desktop/AI/quantification/full_data/computing_data.py"
+
+while True:
+    try:
+        result = subprocess.run(['python3', script_path], check=True)
+        if result.returncode == 0:
+            print("Python script executed successfully.")
+            break
+    except subprocess.CalledProcessError:
+        print("Python script failed. Retrying...")
+        time.sleep(1)  # 等待1秒后重试
