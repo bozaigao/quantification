a05e46086adfd6355647a3fea80c50781a4a1a72
HeYanbo <bozaigao.a@gmail.com>
2024-08-15 18:16:02 +0800

    update

--- full_data/dragon_strategy.py
+++ full_data/dragon_strategy.py
@@ -493,7 +493,7 @@ def strategy(pre_date,date):
                     json.dump(reverseData(dragon_log_data), file,ensure_ascii=False,  indent=4) 
 
 if forecast:
-   strategy('2024-08-14','2024-08-15')
+   strategy('2024-08-15','2024-08-16')
 else:
     for idx, date in enumerate(dates[1:]):
         strategy(str(get_previous_trading_day(datetime.strptime(date, '%Y-%m-%d').date())),date)
